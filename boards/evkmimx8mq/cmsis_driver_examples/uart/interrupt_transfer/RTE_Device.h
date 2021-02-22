85bfc1f19079b106be1e748bcfda819cef82a8ad
Frodo Lai <frodo_lai@bcmcom.com>
2021-02-21 23:42:42 -0800

    update cmsis_driver_examples to UART3
    Signed-off-by: Frodo Lai <frodo_lai@bcmcom.com>

--- boards/evkmimx8mq/cmsis_driver_examples/uart/interrupt_transfer/RTE_Device.h
+++ boards/evkmimx8mq/cmsis_driver_examples/uart/interrupt_transfer/RTE_Device.h
@@ -8,19 +8,19 @@
 #ifndef _RTE_DEVICE_H
 #define _RTE_DEVICE_H
 
-extern void UART2_InitPins();
-extern void UART2_DeinitPins();
+extern void UART3_InitPins();
+extern void UART3_DeinitPins();
 
 /* Driver name mapping. */
 /* User needs to provide the implementation of UARTX_GetFreq/UARTX_InitPins/UARTX_DeinitPins for the enabled UART
  * instance. */
-#define RTE_USART2            1
-#define RTE_USART2_PIN_INIT   UART2_InitPins
-#define RTE_USART2_PIN_DEINIT UART2_DeinitPins
-#define RTE_USART2_DMA_EN     0
+#define RTE_USART3            1
+#define RTE_USART3_PIN_INIT   UART3_InitPins
+#define RTE_USART3_PIN_DEINIT UART3_DeinitPins
+#define RTE_USART3_DMA_EN     0
 
 /* UART configuration. */
 #define USART_RX_BUFFER_LEN     64
-#define USART2_RX_BUFFER_ENABLE 1
+#define USART3_RX_BUFFER_ENABLE 1
 
 #endif /* _RTE_DEVICE_H */
--- boards/evkmimx8mq/cmsis_driver_examples/uart/interrupt_transfer/cmsis_usart_interrupt_transfer.c
+++ boards/evkmimx8mq/cmsis_driver_examples/uart/interrupt_transfer/cmsis_usart_interrupt_transfer.c
@@ -14,7 +14,7 @@
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
-#define DEMO_USART Driver_USART2
+#define DEMO_USART Driver_USART3
 #define ECHO_BUFFER_LENGTH 8
 /*******************************************************************************
  * Prototypes
@@ -37,7 +37,7 @@ volatile bool rxOnGoing                = false;
  * Code
  ******************************************************************************/
 
-uint32_t UART2_GetFreq(void)
+uint32_t UART3_GetFreq(void)
 {
     return BOARD_DEBUG_UART_CLK_FREQ;
 }
